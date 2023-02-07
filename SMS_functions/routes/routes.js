90cab9ef62cab50472e5f8560f72eb63ac22a0f9
Lauryn Laeticia Kantono <87691653+Laurynnnn@users.noreply.github.com>
2023-02-07 13:50:22 +0300

    Add files via upload

--- SMS_functions/routes/routes.js
+++ SMS_functions/routes/routes.js
@@ -1,1 +1,5 @@
-
+const express=require('express')
+const router=express.Router()
+const {sendMessage}=require('../controllers/controller')
+router.post('/number',sendMessage)
+module.exports=router
